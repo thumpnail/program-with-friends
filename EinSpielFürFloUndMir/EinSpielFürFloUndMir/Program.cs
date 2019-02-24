e658f6e0fb88eea2e274a6f312eaf8ee30b2230c
Fenex <friedewald24@gmail.com>
2019-02-24 01:22:28 +0100

    12

--- EinSpielFürFloUndMir/EinSpielFürFloUndMir/Program.cs
+++ EinSpielFürFloUndMir/EinSpielFürFloUndMir/Program.cs
@@ -8,7 +8,7 @@ namespace EinSpielFürFloUndMir {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine();
-            Console .WriteLine("einfach moritz reicht...") ;
+            Console .WriteLine("einfach moritz reicht... test") ;
         }
     }
 }
